bb141339cca577c4fad00feaed5172e82d2c852b
AlexYavrumov <Alexander001>
2021-10-18 12:01:34 +0300

    Homework_to_Lesson5

---
+++ src/HW_to_Lesson15/WrongLoginException.java
@@ -0,0 +1,18 @@
+package HW_to_Lesson15;
+
+public class WrongLoginException extends RuntimeException{
+    private String detail;
+
+    public WrongLoginException() {
+        detail = "Login is incorrect";
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+        detail = message;
+    }
+    @Override
+    public String toString(){
+        return "WrongLoginException: " + detail;
+    }
+}
